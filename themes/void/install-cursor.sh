d1ecddd39f941b908cae7112c7902de7920e284c
Marc Solé i Bonet <marc24force+github@gmail.com>
2025-01-07 00:57:07 +0100

    [FIX]: Add cursor as default

--- themes/void/install-cursor.sh
+++ themes/void/install-cursor.sh
@@ -2,15 +2,18 @@
 
 mkdir -p ~/.icons
 
-if [ ! -d "$HOME/.icons/ArcMidnight-Cursors" ]; then 
-  echo "- Installing ArcMidnight cursor..."
+CURSOR_NAME="ArcMidnight"
+
+if [ ! -d "$HOME/.icons/$CURSOR_NAME" ]; then 
+  echo "- Installing $CURSOR_NAME cursor..."
   REPO="https://github.com/yeyushengfan258/ArcMidnight-Cursors.git"
   echo "  - Downloading from \"$REPO\""
-  git clone $REPO /tmp/ArcMidnight-Cursors 2> /tmp/ArcMidnight-Cursors.log
+  git clone $REPO /tmp/$CURSOR_NAME 2> /tmp/$CURSOR_NAME.log
   
   echo "  - Installing to \"$HOME/.icons\""
-  mv /tmp/ArcMidnight-Cursors/dist ~/.icons/ArcMidnight-Cursors
+  mv /tmp/$CURSOR_NAME/dist ~/.icons/$CURSOR_NAME
+  echo -e "[Icon Theme]\nName=Default\nComment=Default Cursor Theme\nInherits=$CURSOR_NAME" > $HOME/.icons/default/index.theme
   
   echo "  - Cleaning up..."
-  rm /tmp/ArcMidnight-Cursors{,.log} -rf
+  rm /tmp/$CURSOR_NAME{,.log} -rf
 fi
