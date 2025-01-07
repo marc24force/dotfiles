13ce18658273cbf7f4870adeac9d580d499a09db
Marc Solé i Bonet <marc24force+github@gmail.com>
2025-01-07 04:18:58 +0100

    [NEW]: Fantasy Fire theme

---
+++ themes/fantasy-fire/install-cursor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p ~/.icons
+
+CURSOR_NAME="ArcDusk"
+
+if [ ! -d "$HOME/.icons/$CURSOR_NAME" ]; then 
+  echo "- Installing $CURSOR_NAME cursor..."
+  REPO="https://github.com/yeyushengfan258/ArcDusk-Cursors.git"
+  echo "  - Downloading from \"$REPO\""
+  git clone $REPO /tmp/$CURSOR_NAME 2> /tmp/$CURSOR_NAME.log
+  
+  echo "  - Installing to \"$HOME/.icons\""
+  mv /tmp/$CURSOR_NAME/dist ~/.icons/$CURSOR_NAME
+  echo -e "[Icon Theme]\nName=Default\nComment=Default Cursor Theme\nInherits=$CURSOR_NAME" > $HOME/.icons/default/index.theme
+  
+  echo "  - Cleaning up..."
+  rm /tmp/$CURSOR_NAME{,.log} -rf
+fi
