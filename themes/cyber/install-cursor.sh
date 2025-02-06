1ca9af4fa78c99285c4f66cf2c78cb20715fd5ed
Marc Solé i Bonet <marc24force+github@gmail.com>
2025-02-07 00:18:22 +0100

    [NEW]: Cyber theme (WiP)

---
+++ themes/cyber/install-cursor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p ~/.icons/default
+
+CURSOR_NAME="ArcMidnight"
+
+if [ ! -d "$HOME/.icons/$CURSOR_NAME" ]; then 
+  echo "- Installing $CURSOR_NAME cursor..."
+  REPO="https://github.com/yeyushengfan258/ArcMidnight-Cursors.git"
+  echo "  - Downloading from \"$REPO\""
+  git clone $REPO /tmp/$CURSOR_NAME 2> /tmp/$CURSOR_NAME.log
+  
+  echo "  - Installing to \"$HOME/.icons\""
+  mv /tmp/$CURSOR_NAME/dist ~/.icons/$CURSOR_NAME
+  echo "  - Cleaning up..."
+  rm /tmp/$CURSOR_NAME{,.log} -rf
+fi
+echo -e "[Icon Theme]\nName=Default\nComment=Default Cursor Theme\nInherits=$CURSOR_NAME" > $HOME/.icons/default/index.theme
